025f81c80193f4021a0d99d7e61e6f99042a675b
TheZoq2 <frans.skarman@gmail.com>
2017-05-20 13:00:07 +0200

    Add usage instructions

--- send_ip.sh
+++ send_ip.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Get the BOT_TOKEN variable. This should be your secret telegram bot key
+# This file should contain a BOT_TOKEN variable and a CHANNEL_ID variable
+# If the channel is public, you can use @channel_name but if it is private,
+# you need to use the channel id. 
+#
+# To aquire the channel ID, open the channel in web.telegram.org. The url should
+# look something like https://web.telegram.org/#/im?p=c<NUMBERS>_<MORE_NUMBERS>.
+# Your channel id is "-100<NUMBERS>"
+
 source .env
 
 TELEGRAM_URL=https://api.telegram.org/bot${BOT_TOKEN}
